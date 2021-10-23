18c5bfdc6139bea5e6a1a51a0464a4459bfb2de5
lsmucassi <lindasmucassi@gmail.com>
2021-10-23 20:05:45 +0200

    fix: unused comps

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,16 +2,16 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
-import Button from '../../UI/Button/Button';
+// import Button from '../../UI/Button/Button';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => {
-  const resumeHandler = () => {
-    window.open(
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-bb87c.appspot.com/o/Resume%2FPatrick%20Schadler%20Resume.pdf?alt=media&token=a6d27a3a-8d6d-4763-9ea6-af44e571fb1d',
-      '_blank'
-    );
-  };
+  // const resumeHandler = () => {
+  //   window.open(
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-bb87c.appspot.com/o/Resume%2FPatrick%20Schadler%20Resume.pdf?alt=media&token=a6d27a3a-8d6d-4763-9ea6-af44e571fb1d',
+  //     '_blank'
+  //   );
+  // };
 
   const cssClasses = [classes.Toolbar];
 
