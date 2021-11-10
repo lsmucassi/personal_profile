edcdd5b9bbfbc5baf88ffebd81cbde3c0f6f1421
lsmucassi <lindasmucassi@gmail.com>
2021-11-10 09:20:50 +0200

    title fix

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import pslogo from './../../assets/images/ps-logo.png';
+// import pslogo from './../../assets/images/ps-logo.png';
 import classes from './Logo.module.css';
 
 const logo = () => (
   <div className={classes.Logo}>
-    <img src={pslogo} alt='My Resume' />
+    {/* <img src={pslogo} alt='My Resume' /> */}
   </div>
 );
 
